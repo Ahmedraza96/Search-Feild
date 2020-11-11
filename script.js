57f69122e5106e10624b75c89f432e1ded7d8aaf
Ahmed Raza <50247227+Ahmedraza96@users.noreply.github.com>
2020-11-12 00:44:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#search").keyup(function() {
+        var searchVal = $("#search").val().toLowerCase();
+
+        $(".card  ").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(searchVal) > -1)
+        })
+    })
+})
